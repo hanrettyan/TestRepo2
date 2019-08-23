604d058d773b40ba1a3fd566c40c954879f1b162
hanrettyan <47253726+hanrettyan@users.noreply.github.com>
2019-08-23 12:40:32 -0400

    R Proj and Data
    for test repo2

---
+++ data.R
@@ -0,0 +1,8 @@
+# Access data
+library(readr)
+classdata <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRFdDGgmoI-9prZL45gHOixA4thITZleI_DkEZ49E-JqELRaxn8K46YM1HaBb0bBgkV5Xx-YrxKRgYM/pub?output=csv")
+
+names(classdata) <- c("Class Number", "Class Name", "Grade Received", "Academic Year", "Semester")
+
+View(classdata)
+
