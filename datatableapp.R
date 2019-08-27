afe084aaf40913be87d51132b3ec4fbeed7dd1b8
hanrettyan <47253726+hanrettyan@users.noreply.github.com>
2019-08-27 13:13:03 -0400

    App Dev
    App Dev

--- datatableapp.R
+++ datatableapp.R
@@ -1,32 +1,30 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
+# Access data
+library(readr)
 library(DT)
 
+classdata <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRFdDGgmoI-9prZL45gHOixA4thITZleI_DkEZ49E-JqELRaxn8K46YM1HaBb0bBgkV5Xx-YrxKRgYM/pub?output=csv")
+names(classdata) <- c("Class Number", "Class Name", "Grade Received", "Academic Year", "Semester")
+datatable(classdata)
+
+# Build Shiny App
+library(shiny)
 
 
-# Define UI for application that draws a histogram
+############################
+############ UI ############
+############################
 ui <- basicPage(
-    h2("The Sample Class Data"),
+    h3("The Sample Class Data"),
     DT::dataTableOutput("mysampletable")
 )
 
-    
-    
-
-# Define server logic required to draw a table
+############################
+########## SERVER ##########
+############################
 server <- function(input, output) {
-    output$mysampletable = DT::renderDataTable({classdata})
+    output$mysampletable = DT::renderDataTable({classdata}, filter='top')
 }
-
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
